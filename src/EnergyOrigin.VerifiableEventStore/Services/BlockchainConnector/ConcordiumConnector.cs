097b9e01e00969a926ae788ec734b91c3896ada5
Martin Schmidt <martin@scoding.dk>
2022-10-11 10:01:29 +0000

    agre

--- src/EnergyOrigin.VerifiableEventStore/Services/BlockchainConnector/ConcordiumConnector.cs
+++ src/EnergyOrigin.VerifiableEventStore/Services/BlockchainConnector/ConcordiumConnector.cs
@@ -48,6 +48,9 @@ public class ConcordiumConnector : IBlockchainConnector, IDisposable
 
         var transactionStatus = await concordiumNodeClient.Value.GetTransactionStatusAsync(transactionHash);
 
+        Console.WriteLine("1:" + transactionStatus.ToString());
+        Console.WriteLine("2:" + transactionStatus.Status.ToString());
+
         if (transactionStatus != null
             && transactionStatus.Outcomes != null
             && transactionStatus.Status == TransactionStatusType.Finalized)
