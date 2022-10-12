f2c93e912255472f2d5775553548864bbe2a719d
Martin Schmidt <martin@scoding.dk>
2022-10-12 09:00:57 +0000

    Added test

--- src/EnergyOrigin.VerifiableEventStore.ConcordiumIntegrationTests/ConcordiumTests.cs
+++ src/EnergyOrigin.VerifiableEventStore.ConcordiumIntegrationTests/ConcordiumTests.cs
@@ -12,6 +12,7 @@ public class ConcordiumTests
     const string NodeToken = "rpcadmin";
     const string FakeAccount = "MmU1ZTdlYjYzOWJmODc0MjNiYWM1Nzk2Y2ViMWY3MGU4MDU5Mz";
     const string FakePrivateKey = "2e5e7eb639bf87423bac5796ceb1f70e805e938803e36154e361892214974926";
+    const int FiveSeconds = 5000;
 
     [Fact]
     public async Task GetRandomKnownTransaction_Success()
@@ -43,5 +44,30 @@ public class ConcordiumTests
         var transactionRef = await connector.PublishBytes(System.Text.Encoding.UTF8.GetBytes("Hello world"));
 
         Assert.NotNull(transactionRef);
+    }
+
+    [Fact]
+    public async Task PublishAndWaitForTransaction_Success()
+    {
+        var accountAddress = Environment.GetEnvironmentVariable("AccountAddress");
+        var accountKey = Environment.GetEnvironmentVariable("AccountKey");
+
+        var optionsMock = new Mock<IOptions<ConcordiumOptions>>();
+        optionsMock.Setup(obj => obj.Value).Returns(new ConcordiumOptions(NodeAddress, NodeToken, accountAddress, accountKey));
+        var connector = new ConcordiumConnector(optionsMock.Object);
+
+        var transactionRef = await connector.PublishBytes(System.Text.Encoding.UTF8.GetBytes("Hello world"));
+
+        Block? b;
+        var i = 0;
+        do
+        {
+            await Task.Delay(FiveSeconds);
+            b = await connector.GetBlock(transactionRef);
+        }
+        while (b is null && i++ < 10);
+
+        Assert.NotNull(b);
+        Assert.True(b?.Final);
     }
 }
