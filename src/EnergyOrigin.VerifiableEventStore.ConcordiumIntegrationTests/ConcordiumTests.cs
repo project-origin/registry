43ef2b4255a3deae0e4861d17b4c6e67f434622f
Martin Schmidt <martin@scoding.dk>
2022-10-12 07:51:12 +0000

    Moved tests

--- src/EnergyOrigin.VerifiableEventStore.ConcordiumIntegrationTests/ConcordiumTests.cs
+++ src/EnergyOrigin.VerifiableEventStore.ConcordiumIntegrationTests/ConcordiumTests.cs
@@ -1,6 +1,8 @@
 using EnergyOrigin.VerifiableEventStore.Services.BlockchainConnector;
 using Microsoft.Extensions.Options;
 using NSec.Cryptography;
+using Xunit;
+using Moq;
 
 namespace EnergyOrigin.VerifiableEventStore.Tests;
 
@@ -12,21 +14,19 @@ public class ConcordiumTests
     const string FakePrivateKey = "2e5e7eb639bf87423bac5796ceb1f70e805e938803e36154e361892214974926";
 
     [Fact]
-    public void ConcodiumConnector_Instanciate_Success()
+    public async Task GetRequiredHashes_ValidateNumberOfHashesReturned()
     {
+        var randomKnownTransactionHash = "f8931b7da1f1464453d78e8ab606dee44b3bca00c91170e2fcbcba552da485f4";
+        var knownBlockId = "06a531f87594658eee1aeb369b3e755e5b5bb6a34501aa5d24e2adfa025e7343";
+
         var optionsMock = new Mock<IOptions<ConcordiumOptions>>();
         optionsMock.Setup(obj => obj.Value).Returns(new ConcordiumOptions(NodeAddress, NodeToken, FakeAccount, FakePrivateKey));
         var connector = new ConcordiumConnector(optionsMock.Object);
 
-        Assert.NotNull(connector);
-    }
+        var block = await connector.GetBlock(new TransactionReference(randomKnownTransactionHash));
 
-    [Fact]
-    public void ConcodiumConnector_InvalidKey_Fails()
-    {
-        var optionsMock = new Mock<IOptions<ConcordiumOptions>>();
-        optionsMock.Setup(obj => obj.Value).Returns(new ConcordiumOptions(NodeAddress, NodeToken, FakeAccount, "invalidkey"));
-
-        Assert.ThrowsAny<ArgumentException>(() => new ConcordiumConnector(optionsMock.Object));
+        Assert.NotNull(block);
+        Assert.True(block?.Final);
+        Assert.Equal(knownBlockId, block?.BlockId);
     }
 }
