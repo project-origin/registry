2bd13997deb393dba50c380940f3d842cef61664
Martin Schmidt <martin@scoding.dk>
2022-10-24 08:36:28 +0000

    Added toString on EventId

--- src/ProjectOrigin.VerifiableEventStore/Models/EventId.cs
+++ src/ProjectOrigin.VerifiableEventStore/Models/EventId.cs
@@ -1,3 +1,6 @@
 namespace ProjectOrigin.VerifiableEventStore.Models;
 
-public record EventId(Guid EventStreamId, int Index);
+public record EventId(Guid EventStreamId, int Index)
+{
+    public override string ToString() => $"{EventStreamId}-{Index}";
+}
