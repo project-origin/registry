5196cf9fadb10504d0d8a75af041391aabbae1fa
Martin Schmidt <martin@scoding.dk>
2022-10-24 09:03:28 +0000

    Added ConsumptionIssued

---
+++ src/ProjectOrigin.Electricity/Consumption/ConsumptionCertificate.cs
@@ -0,0 +1,12 @@
+using ProjectOrigin.Electricity.Consumption.Requests;
+using ProjectOrigin.Electricity.Shared.Internal;
+
+namespace ProjectOrigin.Electricity.Consumption;
+
+internal class ConsumptionCertificate
+{
+    public void Apply(ConsumptionIssuedEvent issued)
+    {
+
+    }
+}
