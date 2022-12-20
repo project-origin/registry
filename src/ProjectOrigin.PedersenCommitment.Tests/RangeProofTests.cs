660d441f0a7e864c5edae1ab323b5e2bfaab891b
Martin Schmidt <martin@scoding.dk>
2022-12-20 11:25:08 +0000

    tt

--- src/ProjectOrigin.PedersenCommitment.Tests/RangeProofTests.cs
+++ src/ProjectOrigin.PedersenCommitment.Tests/RangeProofTests.cs
@@ -29,21 +29,14 @@ public class RangeProofTests
     {
         var pc_gens = Generator.Default;
         var blinding = Scalar.Random();
+
         var point = pc_gens.Commit(12, blinding);
-
-        // var bp_gens = new BulletProofGen(64, 1);
-        // var label = Encoding.ASCII.GetBytes("test example");
-
-        // var (proof, commit) = RangeProof.ProveSingle(bp_gens, pc_gens, 7, blinding, 32, label);
-        // var res = proof.VerifySingle(bp_gens, pc_gens, commit, 32, label);
-        // Assert.True(res);
-
-        // var bytes = proof.ToBytes();
-        // var new_proof = RangeProof.FromBytes(bytes);
-        // res = new_proof.VerifySingle(bp_gens, pc_gens, commit, 32, label);
-        // Assert.True(res);
-
         Assert.NotNull(point);
-    }
 
+        var compressed = point.Compress();
+        Assert.NotNull(compressed);
+
+        var decompressed = compressed.Decompress();
+        Assert.Equal(point, decompressed);
+    }
 }
