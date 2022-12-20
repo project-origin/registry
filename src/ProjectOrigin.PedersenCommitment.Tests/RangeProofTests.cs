ff3f5304fa7d5a2603283bfbda90694d6eb061a3
Martin Schmidt <martin@scoding.dk>
2022-12-20 11:30:16 +0000

    c

--- src/ProjectOrigin.PedersenCommitment.Tests/RangeProofTests.cs
+++ src/ProjectOrigin.PedersenCommitment.Tests/RangeProofTests.cs
@@ -13,8 +13,8 @@ public class RangeProofTests
         var label = Encoding.ASCII.GetBytes("test example");
 
         var (proof, commit) = RangeProof.ProveSingle(bp_gens, pc_gens, 7, blinding, 32, label);
-        // var res = proof.VerifySingle(bp_gens, pc_gens, commit, 32, label);
-        // Assert.True(res);
+        var res = proof.VerifySingle(bp_gens, pc_gens, commit, 32, label);
+        Assert.True(res);
 
         // var bytes = proof.ToBytes();
         // var new_proof = RangeProof.FromBytes(bytes);
