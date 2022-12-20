3cbcd5857d7cf1a21c511f6af1e7d852db2ad563
Martin Schmidt <martin@scoding.dk>
2022-12-20 11:24:17 +0000

    t2

--- src/ProjectOrigin.PedersenCommitment.Tests/RangeProofTests.cs
+++ src/ProjectOrigin.PedersenCommitment.Tests/RangeProofTests.cs
@@ -23,4 +23,27 @@ public class RangeProofTests
 
         Assert.NotNull(pc_gens);
     }
+
+    [Fact]
+    public void CreateUsingGenrator()
+    {
+        var pc_gens = Generator.Default;
+        var blinding = Scalar.Random();
+        var point = pc_gens.Commit(12, blinding);
+
+        // var bp_gens = new BulletProofGen(64, 1);
+        // var label = Encoding.ASCII.GetBytes("test example");
+
+        // var (proof, commit) = RangeProof.ProveSingle(bp_gens, pc_gens, 7, blinding, 32, label);
+        // var res = proof.VerifySingle(bp_gens, pc_gens, commit, 32, label);
+        // Assert.True(res);
+
+        // var bytes = proof.ToBytes();
+        // var new_proof = RangeProof.FromBytes(bytes);
+        // res = new_proof.VerifySingle(bp_gens, pc_gens, commit, 32, label);
+        // Assert.True(res);
+
+        Assert.NotNull(point);
+    }
+
 }
