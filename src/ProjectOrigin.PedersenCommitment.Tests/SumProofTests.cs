0c25e39e3c827d989bb81b4c2022bead645a89fe
Mikkel Wienberg Madsen <mikkel.wienberg@alexandra.dk>
2022-12-20 10:57:55 +0100

    Test example of sum, equal etc.

--- src/ProjectOrigin.PedersenCommitment.Tests/SumProofTests.cs
+++ src/ProjectOrigin.PedersenCommitment.Tests/SumProofTests.cs
@@ -19,7 +19,7 @@ public class SumProofTests
     }
 
     [Fact]
-    public void ItWorks()
+    public void Zero()
     {
         var pc_gens = Generator.Default;
         var r = Scalar.Random();
@@ -28,6 +28,53 @@ public class SumProofTests
         var proof = ZeroProof.Prove(pc_gens, r);
 
         Assert.True(proof.Verify(pc_gens, c));
+    }
+
+    [Fact]
+    public void Equal()
+    {
+        var pc_gens = Generator.Default;
+        var r0 = Scalar.Random();
+        var r1 = Scalar.Random();
+        var c0 = pc_gens.Commit(42, r0);
+        var c1 = pc_gens.Commit(42, r1);
+
+        var c = c0 - c1;
+        var r = r0 - r1;
+
+        var proof = ZeroProof.Prove(pc_gens, r);
+        Assert.True(proof.Verify(pc_gens, c));
+    }
+
+    [Fact]
+    public void Sum()
+    {
+        var pc_gens = Generator.Default;
+        var r0 = Scalar.Random();
+        var r1 = Scalar.Random();
+        var r2 = Scalar.Random();
+        var r3 = Scalar.Random();
+        var c0 = pc_gens.Commit(10, r0);
+        var c1 = pc_gens.Commit(3, r1);
+        var c2 = pc_gens.Commit(5, r2);
+        var c3 = pc_gens.Commit(2, r3);
+
+        var c = c0 - (c1 + c2 + c3);
+        var r = r0 - (r1 + r2 + r3);
+
+        var proof = ZeroProof.Prove(pc_gens, r);
+        Assert.True(proof.Verify(pc_gens, c));
+    }
+
+
+    [Fact]
+    public void Wrong()
+    {
+        var pc_gens = Generator.Default;
+        var r = Scalar.Random();
+        var c = pc_gens.Commit(42, r); // None Zero
+        var proof = ZeroProof.Prove(pc_gens, r);
+        Assert.False(proof.Verify(pc_gens, c));
     }
 
 }
