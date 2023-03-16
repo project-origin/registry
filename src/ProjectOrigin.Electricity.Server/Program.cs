7b9a95b3a1cb981b681d950b875156d12b0dd23b
Martin Schmidt <martin@scoding.dk>
2023-03-16 11:58:49 +0000

    failing integration test

--- src/ProjectOrigin.Electricity.Server/Program.cs
+++ src/ProjectOrigin.Electricity.Server/Program.cs
@@ -3,7 +3,7 @@ using ProjectOrigin.Electricity.Server;
 var startup = new Startup();
 
 var builder = WebApplication.CreateBuilder(args);
-startup.ConfigureServices(builder.Services);
+startup.ConfigureServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 startup.Configure(app, builder.Environment);
