2edf01c6ad8f1f7470e078e7b26a946b19db0bea
Martin Schmidt <martin@scoding.dk>
2024-02-02 11:36:09 +0000

    Update TransactionProcessorOptions to use PodName instead of ServerNumber

--- src/ProjectOrigin.Registry.Server/Options/TransactionProcessorOptions.cs
+++ src/ProjectOrigin.Registry.Server/Options/TransactionProcessorOptions.cs
@@ -1,9 +1,27 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace ProjectOrigin.Registry.Server.Options;
 
 public record TransactionProcessorOptions()
 {
+    // To compensate for pre 1.28 kubernetes that does not support apps.kubernetes.io/pod-index
+    public string? PodName
+    {
+        init
+        {
+            // regex to find last integer in a string
+            if (value is not null)
+            {
+                var match = Regex.Match(value, @"\d+$");
+                if (match.Success)
+                {
+                    ServerNumber = int.Parse(match.Value);
+                }
+            }
+        }
+    }
+
     [Required, Range(0, 127)]
     public required int ServerNumber { get; init; }
 
