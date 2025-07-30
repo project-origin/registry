d96e9c8629ac127447b8574f7ff02845f599c29f
sahma19 <55732914+sahma19@users.noreply.github.com>
2025-07-30 02:42:34 +0200

    adjust

--- src/ProjectOrigin.Registry/Repository/Postgres/Scripts/300720250231-remove-old-add-new-index.sql
+++ src/ProjectOrigin.Registry/Repository/Postgres/Scripts/300720250231-remove-old-add-new-index.sql
@@ -1,5 +1,5 @@
-CREATE INDEX CONCURRENTLY idx_blocks_tx_range_inc
+CREATE INDEX idx_blocks_tx_range_inc
     ON blocks USING gist (int8range(from_transaction, to_transaction, '[]'));
 
-DROP INDEX CONCURRENTLY idx_blocks_tx_range;
+DROP INDEX idx_blocks_tx_range;
 ALTER INDEX idx_blocks_tx_range_inc RENAME TO idx_blocks_tx_range;
